a040dc9808ce37c4e44718e92e49156d63fc46d6
Tamas Lustyik <elveestei@gmail.com>
2017-03-04 18:14:47 +0100

    Added extension & unit test target

---
+++ XcodeBlockPasteExtension/SourceEditorCommand.swift
@@ -0,0 +1,21 @@
+//
+//  SourceEditorCommand.swift
+//  XcodeBlockPasteExtension
+//
+//  Created by Tamas Lustyik on 2017. 03. 04..
+//  Copyright © 2017. Tamas Lustyik. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation,
+                 completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
---
+++ XcodeBlockPasteExtension/SourceEditorExtension.swift
@@ -0,0 +1,27 @@
+//
+//  SourceEditorExtension.swift
+//  XcodeBlockPasteExtension
+//
+//  Created by Tamas Lustyik on 2017. 03. 04..
+//  Copyright © 2017. Tamas Lustyik. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+    
+    /*
+    func extensionDidFinishLaunching() {
+        // If your extension needs to do any work at launch, implement this optional method.
+    }
+    */
+    
+    /*
+    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+        return []
+    }
+    */
+    
+}
