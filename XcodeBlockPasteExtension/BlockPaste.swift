3d6c2b192992cbf8915627f43fcfb4bd1ef51d5d
Tamas Lustyik <elveestei@gmail.com>
2017-03-05 00:35:56 +0100

    Implemented app logic

--- XcodeBlockPasteExtension/BlockPaste.swift
+++ XcodeBlockPasteExtension/BlockPaste.swift
@@ -15,7 +15,46 @@ class BlockPaste {
     func paste(_ selections: [String],
                into lines: [String],
                at location: PasteLocation) -> [String] {
-        return lines
+        var updatedLines = lines
+        
+        // 1. extend with empty lines
+        let newLineCount = selections.count - (lines.count - location.line)
+        if newLineCount > 0 {
+            if let lastLine = updatedLines.last, !lastLine.hasSuffix("\n") {
+                updatedLines[updatedLines.count - 1] = lastLine.appending("\n")
+            }
+            updatedLines += Array<String>(repeating: "\n", count: max(0, newLineCount - 1))
+            updatedLines.append("")
+        }
+        
+        // 2. insert selection
+        for s in 0 ..< selections.count {
+            var line = updatedLines[location.line + s]
+            let selection = selections[s]
+            
+            // 2.a. apply padding
+            let insertionIndex: String.CharacterView.Index
+            
+            if let newLineIndex = line.characters.index(of: "\n") {
+                let distance = line.characters.distance(from: line.characters.startIndex, to: newLineIndex)
+                if location.column > distance {
+                    let padding = String(repeating: " ", count: location.column - distance)
+                    line.characters.insert(contentsOf: padding.characters, at: newLineIndex)
+                }
+                insertionIndex = line.characters.index(line.characters.startIndex, offsetBy: location.column)
+            } else if location.column >= line.characters.count {
+                line += String(repeating: " ", count: location.column - line.characters.count)
+                insertionIndex = line.characters.endIndex
+            } else {
+                insertionIndex = line.characters.index(line.characters.startIndex, offsetBy: location.column)
+            }
+            
+            // 2.b. insert selection
+            line.characters.insert(contentsOf: selection.characters, at: insertionIndex)
+            updatedLines[location.line + s] = line
+        }
+        
+        return updatedLines
     }
     
 }
